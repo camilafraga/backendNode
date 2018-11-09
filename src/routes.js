d02b32b09be75ea15854ec00d87c984ac1ecead6
camila.dos.s.fraga <camila.dos.s.fraga@accenture.com>
2018-11-09 14:19:23 -0200

    Meu primeiro back em node

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const routes = express.Router();
+const tweetController = require('./controllers/tweetController');
+const likeController = require('./controllers/LikeController');
+
+routes.get('/tweets', tweetController.index);
+routes.post('/tweets', tweetController.store);
+routes.post('/likes/:id', likeController.store);
+module.exports = routes;
